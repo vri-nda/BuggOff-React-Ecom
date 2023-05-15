7fa0e61febe63fc33ee1143687132fdda7979100
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-05-15 15:04:53 +0530

    fixed all ui

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,15 +7,15 @@ function Banner() {
   return (
     <div className='relative w-[66vw] mt-[1rem]'>
         {/* <div className='absolute w-full h-10 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20' /> */}
-        <Carousel autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={2000}>
+        <Carousel  autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={true} interval={2000}>
         <div>
-            <img loading='lazy' src="/imgc2.png" />
+            <img loading='lazy' src="b1.jpg"  />
         </div>
         <div>
-            <img loading='lazy' src="/imgc1.png" />
+            <img loading='lazy' src="b2.jpg" />
         </div>
         <div>
-            <img loading='lazy' src="/banner2.jpg" />
+            <img loading='lazy' src="/b3.jpg" />
         </div>
         </Carousel>
     </div>
