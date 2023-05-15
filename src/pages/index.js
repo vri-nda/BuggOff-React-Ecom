db403d7d8f68f292023874b69fab170f5c59ebbe
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-05-15 15:36:15 +0530

    added footer and fixed some ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,25 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
+import Footer from "../components/Footer";
 
 export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
         <title>Bugg Off</title>
-        
       </Head>
 
-      
       <Header />
 
-      <main className="flex flex-col max-w-screen-2xl mx-auto" >
-      
-      <div className="flex items-center justify-center">
-      <Banner />
-      </div>
-        
-        
+      <main className="flex flex-col max-w-screen-2xl mx-auto">
+        <div className="flex items-center justify-center">
+          <Banner />
+        </div>
+
         <ProductFeed products={products} />
       </main>
+      <Footer />
     </div>
   );
 }
